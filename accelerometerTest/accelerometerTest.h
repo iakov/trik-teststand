cdfbf730f80b9d5087fa1b82fade2872665f57a3
Roman Kurbatov <roman.kurbatov.1992@gmail.com>
2013-12-07 19:45:07 +0400

    accelerometer test

---
+++ accelerometerTest/accelerometerTest.h
@@ -0,0 +1,49 @@
+/* Copyright 2013 Roman Kurbatov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
+#pragma once
+
+#include <QtGui/QWidget>
+#include <QtGui/QListWidget>
+#include <QtGui/QLabel>
+#include <QtGui/QVBoxLayout>
+#include <QtCore/QEventLoop>
+#include <QtCore/QTimer>
+
+#include "testInterface.h"
+#include "messageBox.h"
+
+class AccelerometerTest : public QWidget, public TestInterface
+{
+	Q_OBJECT
+	Q_INTERFACES(TestInterface)
+
+public:
+	TestInterface::Result run(trikControl::Brick &brick, QStringList &log);
+
+private:
+	trikControl::Sensor3d *mAccelerometer;
+	QStringList *mLog;
+
+	QVBoxLayout mLayout;
+	QLabel mTitleLabel;
+	QListWidget mReadingsList;
+
+	QEventLoop mEventLoop;
+	QTimer mDurationTimer;
+	QTimer mTickTimer;
+
+private slots:
+	void printReading();
+};
